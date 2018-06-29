840c8302c459e6cd3502b9473d6fa2d7e7d7abf4
yxb <soldieryu.dev@gmail.com>
2018-06-29 14:18:56 +0800

    微金所案例完成，主要使用了bootstrap、模板代码

--- js/index.js
+++ js/index.js
@@ -1,3 +1,39 @@
 /**
  * Created by yuxibing on 2018/6/13.
  */
+$(function () {
+    banner();
+});
+var banner = function () {
+
+    var getData = function (callback) {
+        if (window.data) {
+            callback && callback(window.data);
+        } else {
+            $.ajax({
+                type: 'get',
+                url: 'js/data.json',
+                dataType: 'json',
+                data: '',
+                success: function (data) {
+                    window.data = data;
+                    callback && callback(window.data);
+                }
+            });
+        }
+    };
+    var render = function () {
+        getData(function (data) {
+            var isMobile = $(window).width() < 768 ? true : false;
+            var pointHtml = template('pointTemplate', {list: data});
+            var imgHtml = template('imgTemplate', {list: data, isMobile:isMobile});
+            $('.carousel-indicators').html(pointHtml);
+            $('.carousel-inner').html(imgHtml);
+        });
+
+    };
+    $(window).on('resize', function () {
+        render();
+        /*通过js主动触发某个事件*/
+    }).trigger('resize');
+};
