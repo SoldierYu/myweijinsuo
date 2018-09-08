63d8c0053ebe2b5db041bbadc048174388827e19
yxb <soldieryu.dev@gmail.com>
2018-09-08 18:11:51 +0800

    fixbug

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ var banner = function () {
         getData(function (data) {
             var isMobile = $(window).width() < 768 ? true : false;
             var pointHtml = template('pointTemplate', {list: data});
-            var imgHtml = template('imgTemplate', {list: data, isMobile:isMobile});
+            var imgHtml = template('imgTemplate', {list: data, isMobile: isMobile});
             $('.carousel-indicators').html(pointHtml);
             $('.carousel-inner').html(imgHtml);
         });
@@ -36,4 +36,27 @@ var banner = function () {
         render();
         /*通过js主动触发某个事件*/
     }).trigger('resize');
+    var isMove = false;
+    var startX = 0;
+    var distanceX = 0;
+    $('.wjs_banner').on('touchstart', function (e) {
+        startX = e.originalEvent.touches[0].clientX;
+    }).on('touchmove', function (e) {
+        var moveX = e.originalEvent.touches[0].clientX;
+        distanceX = moveX - startX;
+        isMove = true;
+    }).on('touchend', function (e) {
+        if (isMove && Math.abs(distanceX) > 50) {
+            if (distanceX < 0) {
+                //往右滑动
+                $('.carousel').carousel('next');
+            } else {
+                //往左滑动
+                $('.carousel').carousel('prev');
+            }
+        }
+        startX = 0;
+        isMove = false;
+        distanceX = 0;
+    })
 };
